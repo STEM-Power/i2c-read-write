b22b8827b9fe7525865a00da5e1008a1bc1c50bb
STEM-Power <81220298+STEM-Power@users.noreply.github.com>
2021-03-24 09:26:58 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@
 /**
  * I2C Read Write block
  */
-//% weight=100 color=#BB5A5A icon="\f126" block="I2C Read & Write"
+//% weight=100 color=#BB5A5A icon="\f542" block="I2C Read & Write"
 namespace I2C_Read_Write {
     let AT24_I2C_ADDR = 80;
 
